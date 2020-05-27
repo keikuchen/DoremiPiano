ee9f3862761ad14d5aa062cde96d045638d366b9
keikuchen <47465833+keikuchen@users.noreply.github.com>
2020-05-27 16:52:24 +0900

    Initial Commit

---
+++ DoremiPiano/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DoremiPiano
+//
+//  Created by Kei Kashi on 2020/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
