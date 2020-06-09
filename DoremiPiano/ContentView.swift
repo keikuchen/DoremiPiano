cadd5d28911c68caa2fe62a453b681b73ce490dc
keikuchen <47465833+keikuchen@users.noreply.github.com>
2020-06-09 14:02:44 +0900

    12鍵作成、ピッチ変更なしの音声出力追加

--- DoremiPiano/ContentView.swift
+++ DoremiPiano/ContentView.swift
@@ -6,15 +6,110 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    let movableDoLongerKeyNotes = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"]
+    let movableDoShorterKeyNotes = ["デ", "リ", "", "フィ", "サ", "チ"]
+    
+    @State var fixedDoLongerKeyNotes = ["C3", "D", "E", "F", "G", "A", "B"]
+    @State var fixedDoShorterKeyNotes = ["C#/D♭", "D#/E♭", "", "F#/G♭", "G#/A♭", "A#/B♭"]
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom) {
+            Color(red: 64.0 / 255, green: 64.0 / 255, blue: 64.0 / 255)
+                .edgesIgnoringSafeArea(.all)
+            ZStack(alignment: .top) {
+                HStack(spacing: 0.0) {
+                    ForEach(0..<7) { i in
+                        Key(keyColor: Color.white,
+                            textColor: Color.black,
+                            width: 40,
+                            height: 150,
+                            movableDoName: self.movableDoLongerKeyNotes[i],
+                            fixedDoName: self.fixedDoLongerKeyNotes[i],
+                            audioFileName: self.movableDoLongerKeyNotes[i],
+                            audioFileType: "wav"
+                        )
+                    }
+                }
+                HStack(spacing: 20.0) {
+                    ForEach(0..<6) { i in
+                        if i == 2 {
+                            Text("")
+                                .frame(width: 20, height: 100)
+                        } else {
+                            Key(keyColor: Color.black,
+                                textColor: Color.white,
+                                width: 20,
+                                height: 100,
+                                movableDoName: self.movableDoShorterKeyNotes[i],
+                                fixedDoName: self.fixedDoShorterKeyNotes[i],
+                                audioFileName: self.movableDoShorterKeyNotes[i],
+                                audioFileType: "wav"
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+
+struct Key: View {
+    var keyColor: Color?
+    var textColor: Color?
+    var width: CGFloat?
+    var height: CGFloat?
+    
+    let movableDoName: String
+    let fixedDoName: String
+    let audioFileName: String
+    let audioFileType: String
+    
+//    let audioFileName: String
+    @State var player: AVAudioPlayer!
+    
+    var body: some View {
+        Button(action: {
+            print(">> action")
+        }) {
+            VStack {
+                Spacer()
+                Text(movableDoName)
+                    .foregroundColor(textColor)
+                Text(fixedDoName)
+                    .foregroundColor(Color.gray)
+            }
+            .frame(width: width, height: height)
+            .background(keyColor)
+            .overlay(
+                RoundedRectangle(cornerRadius: 0)
+                    .stroke(Color.gray)
+            )
+            .overlay(TouchesHandler(
+                didBeginTouch: {
+                    print(">> did begin")
+                    let url = Bundle.main.path(
+                        forResource: self.audioFileName,
+                        ofType: self.audioFileType
+                    )
+                    self.player = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: url!))
+                    self.player.currentTime = 0.1
+                    self.player.play()
+                },
+                didEndTouch: {
+                    print(">> did end")
+                    self.player.stop()
+                }
+            ))
+        }
     }
 }
---
+++ DoremiPiano/TestCustomTapGesture.swift
@@ -0,0 +1,81 @@
+//
+//  TestCustomTapGesture.swift
+//  DoremiPiano
+//
+//  Created by Kei Kashi on 2020/06/05.
+//
+
+import SwiftUI
+import UIKit
+
+class MyTapGesture : UITapGestureRecognizer {
+
+    var didBeginTouch: (()->Void)?
+    var didEndTouch: (()->Void)?
+
+    init(target: Any?, action: Selector?, didBeginTouch: (()->Void)? = nil, didEndTouch: (()->Void)? = nil) {
+        super.init(target: target, action: action)
+        self.didBeginTouch = didBeginTouch
+        self.didEndTouch = didEndTouch
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
+        self.didBeginTouch?()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesEnded(touches, with: event)
+        self.didEndTouch?()
+    }
+}
+
+struct TouchesHandler: UIViewRepresentable {
+    var didBeginTouch: (()->Void)?
+    var didEndTouch: (()->Void)?
+
+    func makeUIView(context: UIViewRepresentableContext<TouchesHandler>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(context.coordinator.makeGesture(didBegin: didBeginTouch, didEnd: didEndTouch))
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<TouchesHandler>) {
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+
+    class Coordinator {
+        @objc
+        func action(_ sender: Any?) {
+            print("Tapped!")
+        }
+
+        func makeGesture(didBegin: (()->Void)?, didEnd: (()->Void)?) -> MyTapGesture {
+            MyTapGesture(target: self, action: #selector(self.action(_:)), didBeginTouch: didBegin, didEndTouch: didEnd)
+        }
+    }
+    typealias UIViewType = UIView
+}
+
+struct TestCustomTapGesture: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+            .background(Color.yellow)
+            .overlay(TouchesHandler(didBeginTouch: {
+                print(">> did begin")
+            }, didEndTouch: {
+                print("<< did end")
+            }))
+    }
+}
+
+struct TestCustomTapGesture_Previews: PreviewProvider {
+    static var previews: some View {
+        TestCustomTapGesture()
+    }
+}
